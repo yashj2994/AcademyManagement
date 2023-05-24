9da6c412647e71c1b37d18f6bcada87b5fcf0a30
Yash <yashj2994@gmail.com>
2023-05-24 03:28:14 -0700

    new commit

--- TheKiranAcadamystaff/src/main/java/com/staff/api/dao/Dao.java
+++ TheKiranAcadamystaff/src/main/java/com/staff/api/dao/Dao.java
@@ -15,80 +15,77 @@ import com.staff.api.model.Staff;
 
 @Repository
 public class Dao {
-	
+
 	@Autowired
 	SessionFactory sf;
 	private String experiance;
-	
+
 //insert data into table
-	
+
 	public String insertdata(Staff staff) {
 
-		Session se=sf.openSession();
-		Transaction tr=se.beginTransaction();
+		Session se = sf.openSession();
+		Transaction tr = se.beginTransaction();
 		se.save(staff);
-				
+
 		tr.commit();
-		
+
 		se.close();
 		return "Data Inserted";
 	}
-	
-	
+
 //		all info in table
-	
-	
+
 	public List<Staff> getAllData() {
-		Session se=sf.openSession();
-		Criteria cr=se.createCriteria(Staff.class);
-		List<Staff> l= cr.list();
-		
+		Session se = sf.openSession();
+		Criteria cr = se.createCriteria(Staff.class);
+		List<Staff> l = cr.list();
+
 		se.close();
 		return l;
-		
+
 	}
-	
+
 //  	avarage salary in table 
-	
+
 	public List<Staff> avgsal() {
-		Session session=sf.openSession();
-		Criteria cr1=session.createCriteria(Staff.class);
-		
+		Session session = sf.openSession();
+		Criteria cr1 = session.createCriteria(Staff.class);
+
 		cr1.add(Restrictions.gt("salary", 20000));
-		List<Staff> li=cr1.list();
+		List<Staff> li = cr1.list();
 		session.close();
 		return li;
 	}
 
 //		Experiance between table
-	
-	
+
 	public List<Staff> between() {
-		Session session=sf.openSession();
-		Criteria cr2=session.createCriteria(Staff.class);
+		Session session = sf.openSession();
+		Criteria cr2 = session.createCriteria(Staff.class);
 		cr2.add(Restrictions.between("experiance", 10, 20));
-		List<Staff> l=cr2.list();
+		List<Staff> l = cr2.list();
 		session.close();
 		return l;
 	}
-	
+
 //   	maximum salary of table
-	
+
 	public List<Staff> maxsal() {
-		Session se=sf.openSession();
+		Session se = sf.openSession();
 		@SuppressWarnings("deprecation")
-		Criteria cr3=se.createCriteria(Staff.class );
+		Criteria cr3 = se.createCriteria(Staff.class);
 		cr3.addOrder(Order.desc("salary"));
-		List<Staff> l=cr3.list();
+		List<Staff> l = cr3.list();
 		se.close();
 		return l;
 	}
 
 //		upadate staff
-	
+
 	public Staff update(Staff staff) {
-		Session se=sf.openSession();
-		Transaction tr=se.beginTransaction();
+		Session se = sf.openSession();
+		Transaction tr = se.beginTransaction();
 		se.update(staff);
 		tr.commit();
 		se.close();
@@ -96,38 +93,33 @@ public class Dao {
 	}
 
 //  minimum experiance
-	
+
 	public String miniexperi() {
-		Session se=sf.openSession();
-		Criteria cr=se.createCriteria(Staff.class);
+		Session se = sf.openSession();
+		Criteria cr = se.createCriteria(Staff.class);
 		cr.addOrder(Order.asc("experiance"));
 		cr.setMaxResults(1);
-		List<Staff> st=cr.list();
-		String s2=null;
+		List<Staff> st = cr.list();
+		String s2 = null;
 		for (Staff staff : st) {
-			s2=staff.getName();
-			
+			s2 = staff.getName();
+
 		}
 		se.close();
 		return s2;
-		
+
 	}
 
 //	whose profile as trainer
-	
+
 	public List<Staff> profile() {
-		Session se=sf.openSession();
-		Criteria cr=se.createCriteria(Staff.class);
-		cr.add(Restrictions.like("profile", "tainer"));
-		List<Staff> ls=cr.list();
+		Session se = sf.openSession();
+		Criteria cr = se.createCriteria(Staff.class);
+		cr.add(Restrictions.like("profile", "owner"));
+		List<Staff> ls = cr.list();
 		se.close();
 		return ls;
-		
-		
-	}
-	
 
-	
-	
-	
+	}
+
 }
